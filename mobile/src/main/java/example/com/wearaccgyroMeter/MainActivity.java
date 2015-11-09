6fb1402c4672bbdd7886b54dce8990c4a43f873d
Hayato Kimura <prprhyt@outlook.com>
2015-11-10 00:38:23 +0900

    2nd commit

--- mobile/src/main/java/example/com/wearaccgyroMeter/MainActivity.java
+++ mobile/src/main/java/example/com/wearaccgyroMeter/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     private GoogleApiClient mGoogleApiClient;
     private TextView resultview;
-    private float accx[];
     private String resultstr;
 
     @Override
